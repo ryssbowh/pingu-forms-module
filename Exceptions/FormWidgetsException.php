2fbb11919c2948aa6b6908a07b143eb1a00d3f44
Boris Blondin <blondin.boris@gmail.com>
2020-03-14 20:09:35 +0000

    various fixes, filter form

--- Exceptions/FormWidgetsException.php
+++ Exceptions/FormWidgetsException.php
@@ -7,10 +7,20 @@ class FormWidgetsException extends \Exception
     /**
      * @param string $name
      * 
-     * @return FormException
+     * @return FormWidgetsException
      */
-    public static function nothingAvailable(string $class)
+    public static function noWidgets(string $class)
     {
         return new static("No form widget defined for $class, did you forget to register your field ?");
+    }
+
+    /**
+     * @param string $name
+     * 
+     * @return FormWidgetsException
+     */
+    public static function noFilterWidgets(string $class)
+    {
+        return new static("No form filter widget defined for $class, did you forget to register your field ?");
     }
 }
